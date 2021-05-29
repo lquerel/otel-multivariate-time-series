1d7f26ee8eb67ddb8097a76ae2584f244c476186
Laurent Querel <l.querel@f5.com>
2021-05-29 10:27:06 -0700

    Draft of a general purpose event object represented as a collection of equal-length columns.

--- src/event.rs
+++ src/event.rs
@@ -1,5 +1,19 @@
 use crate::opentelemetry::proto::events::v1::ResourceMetrics;
 
+pub struct BatchPolicy {
+    pub max_size: usize,
+    pub max_delay_ms: usize,
+}
+
 pub struct EventCollector {
+}
+
+pub struct BatchHandler {}
+
+impl EventCollector {
+    pub fn batch_handler(schema_url: &str, batch_policy: BatchPolicy) -> BatchHandler {}
+}
+
+impl BatchHandler {
 
 }
